0210150822d16aa2a8808c1856c7dd69bf040899
catiehen <caitlin.tiehen@colorado.edu>
2019-02-13 19:08:22 -0700

    lab04 script comment corrections

--- labs/lab04/lab04_script.R
+++ labs/lab04/lab04_script.R
@@ -8,12 +8,12 @@ for (i in seq(1,10)) {
 
 #step 2 
 #write a for loop that prints to the console his total amount of money each week for the next 8 weeks
-#create variables for original amount, allowance and gum
+#create variables for original amount ("current"), allowance ("allow") and gum ("gum")
 current <- 10
 allow <- 5
 gum <- 1.34 * 2 
 
-#for loop: replace the current value with the current value in savings per week for 8 weeks, print value to console
+#for loop: replace the beginning value with the current value in savings per week for 8 weeks, print value to console
 for (i in seq(1,8)) {
     current <- (current) + (allow - gum)
     print(current)
@@ -37,21 +37,21 @@ for (i in seq(1,7)) {
 K <- 10000
 r <- 0.8
 t <- 1
-#create a vector for n repeating 12 times
+#create a vector for "n" repeating 12 times
 n <- rep(2500,12)
 
-#predict the population abundance over the next twelve years, replace n with n[t] to loop equation twelve times
+#predict the population abundance over the next twelve years, replace "n" with "n[t]" to loop equation twelve times
 for (t in 2:12) {
    n[t] <- n [t-1] + (r * n [t-1] * (K - n [t-1])/K )
 }
-#print n to console
+#print "n" to console
 print(n)
 
 #part 2
 
 #step 5 
 #step 5a
-#create a vector contatining eighteen zeros
+#create a vector contatining eighteen zeros, called "zeros"
 zeros <- rep(0,18)
 #print vector to console to confirm it was accurately created
 print(zeros)
@@ -59,41 +59,41 @@ print(zeros)
 #step 5b
 #create a for loop, iterated eighteen times, that will store three times the i'th value of the iterator variable i in the i'th spot of the vector created in step 5a
 three <- 3 
-#replace the values in zeros to show 3 times the i'th value
+#replace the values in "zeros" to show 3 times the i'th value
 for (i in seq(1,18)) {
     zeros[i] <- three * i
 } 
-#print zeros to console
+#print "zeros" to console
 print(zeros)
 
 #step 5c
-#repeat part a to make a new vector of zeros
+#repeat part a to make a new vector of zeros, called "zeros"
 zeros <- rep(0,18)
-#index the first value of the vector zeros to equal 1
+#replace the first index position of the vector "zeros" to equal 1
 zeros[1] <- 1
-#print the vector zeros to the screen
+#print the vector "zeros" to the screen
 print(zeros)
 #check length of the vector
 length(zeros)
 
 #step 5d
-#begin at index 2 in the zeros vector and tell for loop to run across the length of the vector. using the values stored in that vector, replace these values with 1 + 2 * the previous entry.
+#tell for loop to run across the length of the vector begin at index position 2 in the "zeros" vector. using the values stored in that vector, replace these values with 1 + 2 * the previous index position, denoted by [i-1].
 for (i in 2:length(zeros)) {
     zeros[i] <- 1 + (2 * zeros[i-1])
 }
-#print zeros to console
+#print "zeros" to console
 print(zeros)
 
 #step 6
-#create a vector for the first 20 numbers of the fib sequence
+#create a vector for the first 20 numbers of the fib sequence, called "fib"
 fib <- rep(0,20)
-#second position needs to be 1 for the fib sequence
+#make the second index position equal 1 in the "fib" vector
 fib[2] <- 1
-#run a for loop beginning at the index 3 that will use the values in index 2 and 1 (added together) to replace the values throughout the 18 indexes
+#run a for loop, beginning at the index position 3, that will use the values in index position 1 and 2 (added together) to replace the values throughout the following 18 index positions
 for (x in 3:length(fib)) {
     fib[x] <- fib[x-2] + fib[x-1]
 }
-#print fib to console 
+#print "fib" to console 
 print(fib)
 
 #step 7
